f731ae2f094735a5b5e9d7e245a57a9182a5c7d7
lucassousas <lucas.ssilva1903@gmail.com>
2022-05-05 19:42:25 -0300

    Migrations e Models

---
+++ database/migrations/2022_05_05_222737_create_jogadors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Clube;
+use App\Models\PosicaoJogador;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jogador', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome");
+            $table->datetime("dataNasc");
+            $table->foreignIdFor(Clube::class);
+            $table->foreignIdFor(PosicaoJogador::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jogador');
+    }
+};
